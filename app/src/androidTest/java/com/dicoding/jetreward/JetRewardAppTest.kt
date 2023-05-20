a57c9e4eebe3f3d89733d0f1d88d90df0bd09bad
ricky-kiva <ricky.kiva@gmail.com>
2023-05-20 19:11:13 +0700

    add Tests for NavHost

--- app/src/androidTest/java/com/dicoding/jetreward/JetRewardAppTest.kt
+++ app/src/androidTest/java/com/dicoding/jetreward/JetRewardAppTest.kt
@@ -10,6 +10,7 @@ import com.dicoding.jetreward.model.FakeRewardDataSource
 import com.dicoding.jetreward.ui.navigation.Screen
 import com.dicoding.jetreward.ui.theme.JetRewardTheme
 import com.dicoding.jetreward.utils.assertCurrentRouteName
+import com.dicoding.jetreward.utils.onNodeWithStringId
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -41,6 +42,36 @@ class JetRewardAppTest {
         composeTestRule.onNodeWithText(FakeRewardDataSource.dummyRewards[10].title).performClick()
         navController.assertCurrentRouteName(Screen.DetailReward.route)
         composeTestRule.onNodeWithText(FakeRewardDataSource.dummyRewards[10].title).assertIsDisplayed()
+    }
+
+    @Test
+    fun navHost_bottomNavigation_Working() {
+        composeTestRule.onNodeWithStringId(R.string.menu_cart).performClick()
+        navController.assertCurrentRouteName(Screen.Cart.route)
+        composeTestRule.onNodeWithStringId(R.string.menu_profile).performClick()
+        navController.assertCurrentRouteName(Screen.Profile.route)
+        composeTestRule.onNodeWithStringId(R.string.menu_home).performClick()
+        navController.assertCurrentRouteName(Screen.Home.route)
+    }
+
+    @Test
+    fun navHost_clickItem_navigatesBack() {
+        composeTestRule.onNodeWithTag("RewardList").performScrollToIndex(10)
+        composeTestRule.onNodeWithText(FakeRewardDataSource.dummyRewards[10].title).performClick()
+        navController.assertCurrentRouteName(Screen.DetailReward.route)
+        composeTestRule.onNodeWithContentDescription(composeTestRule.activity.getString(R.string.back)).performClick()
+        navController.assertCurrentRouteName(Screen.Home.route)
+    }
+
+    @Test
+    fun navHost_checkout_rightBackStack() {
+        composeTestRule.onNodeWithText(FakeRewardDataSource.dummyRewards[4].title).performClick()
+        navController.assertCurrentRouteName(Screen.DetailReward.route)
+        composeTestRule.onNodeWithStringId(R.string.plus_symbol).performClick()
+        composeTestRule.onNodeWithContentDescription("Order Button").performClick()
+        navController.assertCurrentRouteName(Screen.Cart.route)
+        composeTestRule.onNodeWithStringId(R.string.menu_home).performClick()
+        navController.assertCurrentRouteName(Screen.Home.route)
     }
 
 }
